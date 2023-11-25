0f07bd1968cf1add8b2ca32df0980465c0adf2cf
EUN JIDAM <127118453+JD12321@users.noreply.github.com>
2023-11-25 12:45:23 +0900

    Update 2480.py

--- Python/2480.py
+++ Python/2480.py
@@ -1,15 +1,12 @@
 f,s,t = map(int, input().split())
 r = 0
 
-if(f==s==t):
+if f==s==t:
     r = 10000+f*1000
-elif(f==s or s==t or t==f):
-    if(f==s):
-        r = 1000+f*100
-    elif(s==t):
-        r = 1000+s*100
-    else:
-        r = 1000+t*100
+elif f==s or t==f:
+    r = 1000+f*100
+elif s==t:
+    r = 1000+t*100
 else:
     r = max(f,s,t)*100
 
